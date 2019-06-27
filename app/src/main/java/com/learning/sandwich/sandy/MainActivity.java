ff37cb275fcc9cfdea0ba503abe0d7f1b6c616e9
Monjoie11 <jeff.franken@gmail.com>
2019-06-27 16:18:23 -0600

    added some new logic behind buttons to get them to do he same thing, but now based in values of shared preferences

--- app/src/main/java/com/learning/sandwich/sandy/MainActivity.java
+++ app/src/main/java/com/learning/sandwich/sandy/MainActivity.java
@@ -1,7 +1,10 @@
 package com.learning.sandwich.sandy;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import androidx.annotation.NonNull;
 //import com.facebook.AccessToken;
 //import com.facebook.CallbackManager;
@@ -9,13 +12,17 @@ import androidx.annotation.NonNull;
 //import com.facebook.FacebookException;
 //import com.facebook.login.LoginManager;
 //import com.facebook.login.LoginResult;
+import androidx.navigation.Navigation;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.preference.PreferenceManager;
 
 
 public class MainActivity extends AppCompatActivity {
+
+
 
 
 
@@ -24,13 +31,17 @@ public class MainActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
+    super.onCreate(  savedInstanceState);
     setContentView(R.layout.activity_main);
-
-
-
+    SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+    SharedPreferences.Editor editor = sharedPref.edit();
+    editor.putBoolean(getString(R.string.saved_tutorial_complete_key), false);
+    editor.apply();
+    //upon start check to see if sandy's been educated. if yes, inflate sandwichImage (capture) fragment, if not inflate response frag to tutorial
+    if (!sharedPref.getBoolean(getString(R.string.saved_tutorial_complete_key), false) == false) {
+      Navigation.findNavController(this, R.id.nav_host_fragment).navigate(R.id.action_responseFragment_to_sandwichImageFragment);
+    }
+    //setContentView(R.layout.sandwich_fragment);
 
   }
-
-
 }
--- app/src/main/java/com/learning/sandwich/sandy/ResponseFragment.java
+++ app/src/main/java/com/learning/sandwich/sandy/ResponseFragment.java
@@ -1,6 +1,7 @@
 package com.learning.sandwich.sandy;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -15,6 +16,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.navigation.Navigation;
+import android.preference.PreferenceManager;
 
 public class ResponseFragment extends Fragment {
 
@@ -23,7 +25,12 @@ public class ResponseFragment extends Fragment {
   private ResponseViewModel mViewModel;
   private TextView responseText;
   private Context context;
+  private SharedPreferences sharedPref;
   private Boolean tutorialComplete;
+  SharedPreferences.Editor editor;
+
+
+
 
   public ResponseFragment() {
     // Required (because I copied straight outta at the movies) empty public constructor
@@ -38,6 +45,8 @@ public class ResponseFragment extends Fragment {
   public void onAttach(Context context) {
     super.onAttach(context);
     this.context = context;
+    sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
+    editor = sharedPref.edit();
   } // again outta movies, but context also seems super important
 
 
@@ -45,27 +54,27 @@ public class ResponseFragment extends Fragment {
   public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
       @Nullable Bundle savedInstanceState) {
     final View view = inflater.inflate(R.layout.response_fragment, container, false);
-    responseText = view.findViewById(R.id.response_text);//I'm going to attempt to use some logic to differentiate the functions of the buttons
+    responseText = view.findViewById(R.id.response_text);//I'm going to attempt to use some logic to differentiate the functions of the buttons:
+    if (!sharedPref.getBoolean(getString(R.string.saved_tutorial_complete_key), false)) {
+      responseText.setText(R.string.first_screen);
+      ImageButton yesButton = view.findViewById(R.id.yes_button);
+      yesButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View viewYes1) {
+          Navigation.findNavController(view)
+              .navigate(R.id.action_responseFragment_to_sandwichImageFragment);
+        }
+      });
 
-    responseText.setText(R.string.first_screen);
-    ImageButton yesButton = view.findViewById(R.id.yes_button);
-    yesButton.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View viewYes1) {
-        /*SandwichImageFragment sandwichImageFragment = new SandwichImageFragment();
-        FragmentManager manager  = getFragmentManager();
-        manager.beginTransaction().replace(R.id.action_responseFragment_to_sandwichImageFragment, sandwichImageFragment, sandwichImageFragment.getTag()).commit();*/
-        Navigation.findNavController(view).navigate(R.id.action_responseFragment_to_sandwichImageFragment);
-      }
-    });
-
-    ImageButton noButton = view.findViewById(R.id.no_button);
-    noButton.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View viewNo1) {
-        Toast.makeText(context, "Watch where you put that finger. I said touch my buns.", Toast.LENGTH_LONG).show();
-      }
-    });// I got this to work using that handy context I created.
+      ImageButton noButton = view.findViewById(R.id.no_button);
+      noButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View viewNo1) {
+          Toast.makeText(context, "Watch where you put that finger. I said touch my buns.",
+              Toast.LENGTH_LONG).show();
+        }
+      });// I got this to work using that handy context I created.
+    }
 
     return view;
   }
@@ -75,6 +84,15 @@ public class ResponseFragment extends Fragment {
     super.onActivityCreated(savedInstanceState);
     mViewModel = ViewModelProviders.of(this).get(ResponseViewModel.class);
     // TODO: Use the ViewModel
+  }
+
+
+  public Boolean getTutorialComplete() {
+    return tutorialComplete;
+  }
+
+  public void setTutorialComplete(Boolean tutorialComplete) {
+    this.tutorialComplete = tutorialComplete;
   }
 }
 
